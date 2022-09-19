3e08bcbea0b5d8ce74617d54df1124bb9c959a3f
VeronikaAleshkova <105292660+VeronikaAleshkova@users.noreply.github.com>
2022-09-19 22:55:37 +0300

    add tasks 7-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,45 @@
+// Напиши скрипт управления формой логина.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  // console.dir(evt.currentTarget.elements);
+  // console.dir(evt.currentTarget.elements.email.value);
+  // console.dir(evt.currentTarget.elements.password.value);
+
+  const {email, password} = evt.currentTarget.elements;
+  // console.log(email.value);
+  // console.log(password.value);
+
+  if (email.value.trim() === '' || password.value.trim() === '') {
+    alert("Bсе поля должны быть заполнены!");
+  }
+
+  const dataObj = {
+    email: email.value,
+    password: password.value
+  }
+  console.log(dataObj);
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,28 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', onClick);
+
+function onClick(evt) {
+  let currentColor;
+  currentColor = getRandomHexColor();
+  console.log(currentColor);
+  body.style.backgroundColor = currentColor;
+  span.textContent = currentColor;
+};
+
